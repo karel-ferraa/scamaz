29edf5050c5184bb7997a1cb595ab7466e83cdd9
MathieuChabrun <151022078+MathieuChabrun@users.noreply.github.com>
2026-04-22 11:05:40 +0200

    Add files via upload

---
+++ niv.gd
@@ -0,0 +1,82 @@
+extends Node2D
+
+var round = 0
+var my_score = 0
+var peer_score = 0
+var my_choice = null   
+var peer_choice = null  # ← null et non false
+
+@onready var btn_mettre = $HBoxContainer/Button
+@onready var btn_pas_mettre = $HBoxContainer2/Button
+@onready var label_score = $Label_Score
+@onready var label_resultat = $Label_Résultat
+@onready var label_attente = $Label_Attente
+
+
+func _ready():
+	btn_mettre.pressed.connect(_on_mettre)
+	btn_pas_mettre.pressed.connect(_on_pas_mettre)
+	label_resultat.text = ""
+	_maj_score()
+
+func _on_mettre():
+	_faire_choix(true)
+
+func _on_pas_mettre():
+	_faire_choix(false)
+
+func _faire_choix(choix: bool):
+	if my_choice != null:
+		return 
+	my_choice = choix
+	btn_mettre.disabled = true
+	btn_pas_mettre.disabled = true
+
+	# MODE LOCAL 
+	var choix_adverse = [true, false].pick_random()
+	_recevoir_choix_adverse(choix_adverse)
+
+func _recevoir_choix_adverse(choix: bool):
+	peer_choice = choix
+	if my_choice != null:
+		_resoudre_manche()
+
+func _resoudre_manche():
+	var texte: String
+	
+	if my_choice == true and peer_choice == true:
+		my_score += 2
+		peer_score += 2
+		texte = "Vous avez tous les deux mis ! +2 pièces"
+	elif my_choice == false and peer_choice == false:
+		texte = "Personne n'a mis. +0 pièce"
+	elif my_choice == true and peer_choice == false:
+		peer_score += 3
+		my_score -= 1
+		texte = "Tu as mis, lui non... -1 pièce"
+	else:
+		my_score += 3
+		peer_score -= 1
+		texte = "Il a mis, toi non ! +3 pièces"
+	
+	label_resultat.text = texte
+	_maj_score()
+
+	# Attendre 2 secondes avant nouvelle manche
+	await get_tree().create_timer(2.0).timeout
+	if round < 5 :
+		round +=1
+		_nouvelle_manche()
+	else : 
+		label_attente.text = "Partie Terminé"
+	
+
+func _nouvelle_manche():
+	my_choice = null
+	peer_choice = null
+	label_resultat.text = ""
+	btn_mettre.disabled = false
+	btn_pas_mettre.disabled = false
+
+func _maj_score():
+	label_score.text = "Toi : %d  |  Adversaire : %d" % [my_score, peer_score]
